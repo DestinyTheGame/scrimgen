75e74354641ca61f809295444d47c058a8452641
Sahand Johansen <sahand.johansen@gmail.com>
2021-09-30 21:47:21 +0200

    Add map-icons. Use Nomad themed background gradient. (#2)
    * Add map-icons. Use Nomad-ish themed background gradient. Try to make it mobile friendly
    
    * Rename images in snake_case syntax. Use rem instead of px in CSS. Other minor CSS changes
    
    * Crop image on phones to prevent need for scrolling.
    
    * Fix viewport issues for background.
    
    * Use correct identifier for header-text. Update font size for smaller screens.
    
    Co-authored-by: SahandJ <sj@computas.com>

--- scripts/map-randomizer.js
+++ scripts/map-randomizer.js
@@ -17,41 +17,43 @@
 
   const target = document.getElementById('target');
 
-  //
   // Current selection of maps that are available in the game.
   //
   // - `name` Human readable name of the map.
   // - `size` indicates the physical size of map, in t-shirt size.
   // - `modes` List of modes this map can be used in.
-  //
+  // - `image` Name of the image icon associated with the given map
   const maps = [
-    {name: 'Altar of Flame', size: 'm', modes: ['clash']},
-    {name: 'Anomaly', size: 's', modes: ['clash']},
-    {name: 'Bannerfall', size: 'm', modes: ['clash']},
-    {name: 'Burnout', size: 's', modes: ['clash']},
-    {name: 'Cauldron', size: 's', modes: ['clash']},
-    {name: 'Convergence', size: 'm', modes: ['clash']},
-    {name: 'Dead Cliffs', size: 's', modes: ['clash']},
-    {name: 'Endless Vale', size: 's', modes: ['clash']},
-    {name: 'Exodus Blue', size: 's', modes: ['clash']},
-    {name: 'Fortress', size: 'l', modes: ['clash']},
-    {name: 'Fragment', size: 'l', modes: ['clash']},
-    {name: 'Javelin-4', size: 's', modes: ['clash']},
-    {name: 'Midtown', size: 'l', modes: ['clash']},
-    {name: 'Pacifica', size: 'm', modes: ['clash']},
-    {name: 'Radiant Cliffs', size: 'l', modes: ['clash']},
-    {name: 'Rusted Lands', size: 'm', modes: ['clash']},
-    {name: 'Twilight Gap', size: 'm', modes: ['clash']},
-    {name: 'Widow\'s Court', size: 'l', modes: ['clash']},
-    {name: 'Wormhaven', size: 'm', modes: ['clash']}
+    {name: 'Altar of Flame', size: 'm', modes: ['clash'], image: 'altar_of_flame.jpg'},
+    {name: 'Bannerfall', size: 'm', modes: ['clash'], image: 'bannerfall.jpg'},
+    {name: 'Cauldron', size: 's', modes: ['clash'], image: 'cauldron.jpg'},
+    {name: 'Convergence', size: 'm', modes: ['clash'], image: 'convergence.jpg'},
+    {name: 'Distant Shore', size: 'm', modes: ['clash'], image: 'distant_shore.jpg'},
+    {name: 'Endless Vale', size: 's', modes: ['clash'], image: 'endless_vale.jpg'},
+    {name: 'Exodus Blue', size: 's', modes: ['clash'], image: 'exodus_blue.jpg'},
+    {name: 'Fragment', size: 'l', modes: ['clash'], image: 'fragment.jpg'},
+    {name: 'Javelin-4', size: 's', modes: ['clash'], image: 'javelin_4.jpg'},
+    {name: 'Midtown', size: 'l', modes: ['clash'], image: 'midtown.jpg'},
+    {name: 'Pacifica', size: 'm', modes: ['clash'], image: 'pacifica.jpg'},
+    {name: 'Radiant Cliffs', size: 'l', modes: ['clash'], image: 'radiant_cliffs.jpg'},
+    {name: 'Rusted Lands', size: 'm', modes: ['clash'], image: 'rusted_lands.jpg'},
+    {name: 'The Anomaly', size: 's', modes: ['clash'], image: 'the_anomaly.jpg'},
+    {name: 'The Burnout', size: 's', modes: ['clash'], image: 'the_burnout.jpg'},
+    {name: 'The Dead Cliffs', size: 's', modes: ['clash'], image: 'the_dead_cliffs.jpg'},
+    {name: 'The Fortress', size: 'l', modes: ['clash'], image: 'the_fortress.jpg'},
+    {name: 'Twilight Gap', size: 'm', modes: ['clash'], image: 'twilight_gap.jpg'},
+    {name: 'Widow\'s Court', size: 'l', modes: ['clash'], image: 'widows_court.jpg'},
+    {name: 'Wormhaven', size: 'm', modes: ['clash'], image: 'wormhaven.jpg'}
   ];
 
   /**
    * Render the current map selection based on the pre-defined filters.
    *
+   * @param {Event} [event] - An optional Event argument for handling event listeners
    * @private
    */
-  function render() {
+  function render(event = null) {
+    if (event !== null) event.preventDefault();
     const size = Array.from(document.querySelectorAll('input[name="size"]:checked')).map((elem) => elem.value);
     const amount = document.getElementById('amount').value;
 
@@ -64,23 +66,28 @@
       });
     });
 
-    // Shuffle the list of relevant maps. The first three maps are chosen
+    // Shuffle the list of relevant maps. The first N (`amount`) maps are chosen as the selection
     shuffleArray(available);
     const selection = available.slice(0, amount);
 
-    if (!selection.length) return target.innerHTML = `
-          <div class="empty">
-            <div class="empty-icon">
-              <span class="icon icon-emoji"></span>
-            </div>
-            <p class="empty-title h5">Congratulations</p>
-            <p class="empty-subtitle">You've successfully selected no maps, you muppet</p>
+    if (!selection.length) {
+      target.innerHTML =
+        `<div class="empty">
+          <div class="empty-icon">
+            <span class="icon icon-emoji"></span>
           </div>
-        `;
+        <p class="empty-title h5">Congratulations</p>
+        <p class="empty-subtitle">You've successfully selected no maps, you muppet</p>
+        </div>`;
+      return;
+    }
 
-    target.innerHTML = '<ol>' + selection.map(function format(map) {
-      return `<li><span class="chip">${map.name}</span></li>`;
-    }).join('') + '</ol>';
+    target.innerHTML = selection.map((entry, idx) => {
+      return `<div class="img-container">
+                <img src="images/maps/${entry.image}" alt=${entry.name} class="img-responsive" aria-hidden="true"/>
+                <div class="img-text-header">${idx + 1} | ${entry.name}</div>
+              </div>`
+    }).join('');
   }
 
 
@@ -92,5 +99,6 @@
   });
 
   document.getElementById('again').addEventListener('click', render);
+  document.forms[0].addEventListener('submit', render)
   return render();
 })();
